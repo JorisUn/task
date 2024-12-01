5a7b568e5ea3c0cc418311dc9bc4143bb0acdfac
JorisUn <joris.unikas21@gmail.com>
2024-12-01 21:11:28 +0200

    task list option implementation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # task
+- Read tasks from a file
+- Create a file if it doesn't exist
+- Save tasks to file at the end of the program
++ Print tasks to the screen
--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,13 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
+type Task struct{
+    name string
+    description string
+    completed bool
+}
 
 func main(){
 
@@ -17,22 +23,64 @@ func main(){
 
     /** Reading data from a file **/
     file, e := os.Open("../data.csv")
-    if e == os.ErrNotExist {
-        // Create file to save data at the end of the program
-        os.Create("../data.csv")
-    } else if(e == nil){
-        // If file already exists read data
-        defer file.Close()
-        reader := csv.NewReader(file)
-        lines, e := reader.ReadAll()
-        handle(e)
-    } else {
-        panic(e)
+    handle(e)
+    defer file.Close()
+    reader := csv.NewReader(file)
+    dataFormat, _ :=reader.Read()
+    _ = dataFormat
+    lines, e := reader.ReadAll()
+    handle(e)
+    TaskArr := readToArr(lines)
+
+    /** CLI options **/
+    if len(cmdArgs) > 1 {
+        switch cmdArgs[1] {
+        case "list":
+            shouldPrint := false
+            for _, t := range TaskArr{
+                if t.completed == false{
+                    shouldPrint = true
+                }
+            }
+            if shouldPrint {
+                fmt.Println("Tasks to complete:")
+                i := 1
+                for _, t := range TaskArr {
+                    if !t.completed {
+                        fmt.Printf("%v. %v: %v\n", i, t.name, t.description)
+                        i++;
+                    }
+                }
+            } else if len(TaskArr) == 0{
+                fmt.Println("Tasks list is empty.")
+            } else {
+                fmt.Println("All tasks have been completed.")
+            }
+        }
     }
+}
+
+// Task contructor
+func NewTask(name, description string, completed bool) *Task{
+    t := Task{name:name}
+    t.description = description
+    t.completed = completed
+    return &t
 }
 
 func handle(e error){
     if(e != nil){
         panic(e)
     }
+}
+
+// Reads input string matrix to Task array
+func readToArr(lines[][] string) []Task {
+    t := make([]Task, 0)
+    for _, v := range lines {
+        completed, e := strconv.ParseBool(v[2])
+        handle(e)
+        t = append(t, *NewTask(v[0], v[1], completed)) 
+    }
+    return t
 }
